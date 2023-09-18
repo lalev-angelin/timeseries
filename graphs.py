542268832a0b2cd4b39d49c4275437b445c4ce0a
Angelin Lalev <lalev@uni-svishtov.bg>
2023-09-18 18:03:35 +0300

    Refactoring...

--- graphs.py
+++ graphs.py
@@ -5,7 +5,6 @@ Created on Sun Sep 17 22:15:25 2023
 
 @author: ownjo
 """
-from pandas import read_csv
 from matplotlib.backends.backend_pdf import PdfPages
 import matplotlib.pyplot as plt
 from MovingAveragePredictionMethod import MovingAveragePredictionMethod
@@ -13,15 +12,36 @@ from ExponentialPredictionMethod import ExponentialPredictionMethod
 from DoubleExponentialPredictionMethod import DoubleExponentialPredictionMethod
 from TripleExponentialPredictionMethod import TripleExponentialPredictionMethod
 from SimpleRNNPredictionMethod import SimpleRNNPredictionMethod
+import pandas as pd
 
 ### ЗАРЕЖДАНЕ НА ДАННИТЕ
 
-data = read_csv('m1.csv', sep=',', decimal=".")
+data = pd.read_csv('m1.csv', sep=',', decimal=".")
 #print(data)
 
 pdf = PdfPages("graphs_m1.pdf")
 
-for q in range (0, len (data)):
+types = {
+    'name': 'object',
+    'datapoints': 'int32',
+    'future_predictions': 'int32',
+    'ma_mape': 'float64',
+    'ma_wmape': 'float64',
+    'es_mape': 'float64',
+    'es_wmape': 'float64',
+    'holt_mape': 'float64',
+    'holt_wmape': 'float64',
+    'holtwinters_mape': 'float64',
+    'simple_rnn_mape': 'float64', 
+    'simple_rnn_wmape': 'float64'
+   }
+
+resultData = pd.DataFrame(columns=types.keys())
+# for c in resultData.columns:
+#     resultData[c] = resultData[c].astype(types[c])
+
+#for q in range (0, len (data)):
+for q in range (260, 270):
 
     # Номер на ред с данни
     rowno = q
@@ -53,6 +73,11 @@ for q in range (0, len (data)):
     # Някои проверки за валидност 
     assert datapoints + future_predictions == len(row.values)
     
+    
+    resultData.at[q, 'name']=series_name
+    resultData.at[q,'datapoints']=datapoints
+    resultData.at[q, 'future_predictions']=future_predictions
+   
     # Движеща се средна
     average = MovingAveragePredictionMethod(row, datapoints, window=3)
     prediction = average.predict()
@@ -67,12 +92,18 @@ for q in range (0, len (data)):
     plt.plot(prediction, color="orange", label="Moving average 3, MAPE=%s, wMAPE=%s" % (average.computeMAPE(), average.computeWMAPE()))
     plt.legend()
 
+    resultData[q, 'ma_mape']=average.computeMAPE()
+    resultData[q, 'ma_wmape']=average.computeWMAPE()
+
     # Просто експоненциално изглаждане
     exponential = ExponentialPredictionMethod(row, datapoints)
     prediction = exponential.predict()
     plt.plot(prediction, color="magenta", label="Exp. smoothing $\\alpha=%s$, MAPE=%s, wMAPE=%s" % 
               (exponential.hwresults.params['smoothing_level'], exponential.computeMAPE(), exponential.computeWMAPE()))
     plt.legend()
+
+    resultData.at[q, 'es_mape']=exponential.computeMAPE()
+    resultData.at[q, 'es_wmape']=exponential.computeWMAPE()
 
     
     # Двойно експоненциално изглаждане
@@ -81,6 +112,9 @@ for q in range (0, len (data)):
     plt.plot(prediction, color="black", label="Exp. smoothing $\\alpha=%s$, $\\beta=%s$, MAPE=%s, wMAPE=%s" % 
               (holt.hwresults.params['smoothing_level'], holt.hwresults.params['smoothing_trend'], holt.computeMAPE(), holt.computeWMAPE()))
     plt.legend()
+
+    resultData.at[q, 'holt_mape']=holt.computeMAPE()
+    resultData.at[q, 'holt_wmape']=holt.computeWMAPE()
 
     # Тройно експоненциално изглаждане
     if seasonality>1:
@@ -93,12 +127,19 @@ for q in range (0, len (data)):
                holtwinters.hwresults.params['smoothing_seasonal'], holtwinters.computeMAPE(), holtwinters.computeWMAPE()))
     plt.legend()
 
+    resultData.at[q, 'holtwinters_mape']=holtwinters.computeMAPE()
+    resultData.at[q, 'holtwinters_wmape']=holtwinters.computeWMAPE()
+
     # Проста RNN
     simpleRNN = SimpleRNNPredictionMethod(row, datapoints)
     prediction = simpleRNN.predict()
     plt.plot(prediction, color="cyan", label="Simple RNN, MAPE=%s, wMAPE=%s" % (simpleRNN.computeMAPE(), simpleRNN.computeWMAPE()))
     plt.legend()
+    
+    resultData.at[q, 'prediction_mape']=simpleRNN.computeMAPE()
+    resultData.at[q, 'prediction_wmape']=simpleRNN.computeWMAPE()
 
     pdf.savefig(fig)
 
 pdf.close()
+resultData.to_csv('data.csv')
