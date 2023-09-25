376e6f74b27c0b629f3162395c4331c4ef815c17
Angelin Lalev <a.lalev@uni-svishtov.bg>
2023-09-25 16:21:30 +0300

    Adding features to figures_plotter.py

--- tools/figures_plotter.py
+++ tools/figures_plotter.py
@@ -16,6 +16,12 @@ def compileStats(filename):
     i=1
     plotMav = True
     plotHoltWinters = True
+    plotSimpleRNN = True
+    plotSimpleLSTM = True
+    plotCombinedRNN = True
+    plotAveragedRNN = True
+    plotDetrendedRNN = True
+    plotBoxCoxRNN = True
     
     for directory in os.listdir():
         if not os.path.isdir(directory):
@@ -46,19 +52,67 @@ def compileStats(filename):
             plt.plot(data['prediction'], color="orange", label="SMA(3)")
             plt.legend()
 
-
+        # Двойно или тройно експоненциално изглаждане (зависи дали има данни за сезонност)
         if (plotHoltWinters):
             f = open(directory+"/hw.json")
             data = json.load(f)
             f.close()
-            alpha = data['params']['alpha']
-            beta = data['params']['beta']
-            gamma = data['params']['gamma']
-            plt.plot(data['prediction'], color="red", label="Holt-Winters $\\alpha=%s$, $\\beta=%s$ $\\gamma=%s$"% 
-                     (alpha, beta, gamma))
+            alpha = data['parameters']['smooth_alpha']
+            beta = data['parameters']['smooth_beta']
+            gamma = data['parameters']['smooth_gamma']
+            plt.plot(data['prediction'], color="red", label="Holt-Winters $\\alpha\\approx$%s, $\\beta\\approx$%s $\\gamma\\approx$%s"% 
+                     (round(alpha,4), round(beta,4), round(gamma,4)))
             plt.legend()
             
+        # Проста RNN
+        if (plotSimpleRNN):  
+            f = open(directory+"/srnn.json")
+            data = json.load(f)
+            f.close()
+            plt.plot(data['prediction'], color="green", label="Simple RNN")
+            plt.legend()
+            
+        # Проста LSTM     
+        if (plotSimpleLSTM):  
+            f = open(directory+"/slstm.json")
+            data = json.load(f)
+            f.close()
+            plt.plot(data['prediction'], color="brown", label="Simple LSTM")
+            plt.legend()
         
+        # Комбиниран RNN
+        if (plotCombinedRNN):
+            f = open(directory+"/crnn.json")
+            data = json.load(f)
+            f.close()
+            plt.plot(data['prediction'], color="darkgray", label="Combined RNN")
+            plt.legend()
+
+        # Усреднен RNN
+        if (plotAveragedRNN):
+            f = open(directory+"/arnn.json")
+            data = json.load(f)
+            f.close()
+            plt.plot(data['prediction'], color="yellow", label="Averaged RNN")
+            plt.legend()
+
+        # Детренднат RNN
+        if (plotDetrendedRNN):
+            f = open(directory+"/detrended_rnn.json")
+            data = json.load(f)
+            f.close()
+            plt.plot(data['prediction'], color="green", linestyle="--", label="Linear Detrend + RNN")
+            plt.legend()
+            
+        # BoxCox + RNN
+        if (plotBoxCoxRNN):
+            f = open(directory+"/boxcox_rnn.json")
+            data = json.load(f)
+            f.close()
+            plt.plot(data['prediction'], color="green", linestyle="-.", label="BoxCox + RNN")
+            plt.legend()
+                        
+            
         plt.savefig(directory+"/"+filename)
 
           
